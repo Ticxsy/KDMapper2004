2982a41d140a6ccd7232313c8d2e6032affb6a73
Ticxsy <64078606+Ticxsy@users.noreply.github.com>
2020-07-30 21:01:22 +0200

    Add files via upload

---
+++ kdmapper.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+#include <stdint.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include "ntdll.h"
+
+#include "portable_executable.hpp"
+#include "utils.hpp"
+#include "nt.hpp"
+#include "intel_driver.hpp"
+#include "Driver.h"
+
+namespace kdmapper
+{
+	uint64_t MaprawData(HANDLE iqvw64e_device_handle);
+	uint64_t MapDriver(HANDLE iqvw64e_device_handle, const std::string& driver_path);
+	void RelocateImageByDelta(portable_executable::vec_relocs relocs, const uint64_t delta);
+	bool ResolveImports(HANDLE iqvw64e_device_handle, portable_executable::vec_imports imports);
+	void InitStackCookie(void* base);
+}
